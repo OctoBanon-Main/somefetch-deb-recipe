8ec61395c823d5abefc8f2c47dc8ab7365179c25
Maksim Bengraf <me@octobanon.com>
2025-02-20 19:37:20 +0700

    chore(debian): uploaded recipe for building

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/somefetch/issues
+# Bug-Submit: https://github.com/<user>/somefetch/issues/new
+# Changelog: https://github.com/<user>/somefetch/blob/master/CHANGES
+# Documentation: https://github.com/<user>/somefetch/wiki
+# Repository-Browse: https://github.com/<user>/somefetch
+# Repository: https://github.com/<user>/somefetch.git
